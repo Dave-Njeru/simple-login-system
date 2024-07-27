88d1ed0c8436329c470607d61afc10c48c324eff
Dave-Njeru <davenjeru9@gmail.com>
2024-07-27 13:03:51 +0300

    added more helpers

---
+++ helpers/footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <div class="row">
+        <div class="col-6">
+            <span>© 2024 Lions Club University. All rights reserved.</span>
+        </div>
+        <div class="col-6">
+            <ul>
+                <li><i class="fa fa-youtube"></i></li>
+                <li><i class='fab fa-linkedin-in'></i></li>
+                <li><i class='fab fa-instagram'></i></li>
+                <li><i class='fab fa-twitter'></i></li>
+                <li><i class='fab fa-facebook-f'></i></li>
+            </ul>
+        </div>
+    </div>
+</footer>
---
+++ helpers/header.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="row">
+        <div class="col-1">
+            <img src="https://cdn.pixabay.com/photo/2017/03/18/19/31/lion-2154762_960_720.png" 
+            alt="school logo" class="img-fluid" width="70%">
+        </div>
+        <div class="col-11">
+            <div class="title">LIONS CLUB UNIVERSITY</div>
+        </div>
+    </div>
+</header>
